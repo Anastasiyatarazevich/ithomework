3fb61d422797c401891c57901183e9efb6784ade
Seni <grunin200412@gmail.com>
2019-10-19 11:55:49 +0300

    1.7.6/рисование узоров на Canvas квадрат

--- app/src/main/java/com/example/smt/View.java
+++ app/src/main/java/com/example/smt/View.java
@@ -2,16 +2,17 @@ package com.example.smt;
 
 
 
-import android.os.Bundle;
+
 //import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyView(this));
     }
-}
+}*/
