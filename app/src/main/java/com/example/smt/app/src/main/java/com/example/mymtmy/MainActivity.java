2d949493b8f382b2f43e695936f7ea7f0ecf8e19
anata <anastasiya.tarazevich22@gmail.com>
2019-12-05 19:57:02 +0300

    2.4.5equation

--- app/src/main/java/com/example/smt/app/src/main/java/com/example/mymtmy/MainActivity.java
+++ app/src/main/java/com/example/smt/app/src/main/java/com/example/mymtmy/MainActivity.java
@@ -3,49 +3,33 @@ package com.example.mymtmy;
 
 
 
+import android.app.Activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    // Вызывается при создании Активности
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Инициализирует Активность.
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "create", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "start", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "stop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "pause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "resume", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "destroy", Toast.LENGTH_SHORT).show();
+    /** Вызывается при нажатии пользователем на кнопку Решить */
+    public void solveEquation(View view) {
+        // ax+b=c
+        double a = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_a)).getText().toString());
+        double b = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_b)).getText().toString());
+        double c = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_c)).getText().toString());
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText("" + String.valueOf((c - b) / a));
     }
 
 }
